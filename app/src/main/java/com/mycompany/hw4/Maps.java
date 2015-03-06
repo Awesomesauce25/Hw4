6c6782fce115bb185b96fa5734bf59395a96c902
Awesomesauce25 <cynthia.luu@utexas.edu>
2015-03-06 17:37:52 -0600

    added the maps activity

--- app/src/main/java/com/mycompany/hw4/Maps.java
+++ app/src/main/java/com/mycompany/hw4/Maps.java
@@ -1,28 +1,24 @@
 package com.mycompany.hw4;
 
-
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.app.Activity;
-import android.os.Bundle;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Maps extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maps);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_maps, menu);
         return true;
     }
 
@@ -40,5 +36,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
